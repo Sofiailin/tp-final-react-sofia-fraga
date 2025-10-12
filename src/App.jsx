0e5703a459fefeb50ea2584f1ed30fe668ed10df
Sofi Fraga <sofi.163.sf@gmail.com>
2025-10-11 22:10:36 -0300

    muestra de pokemones y css

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,17 @@
-import { useEffect, useState } from 'react'
 import Navbar from './Componentes/Navbar'
 import Buscador from './Componentes/Buscador'
+import Pokemones from './Componentes/Pokemones'
 
 
 function App() {
   
-  const [pokemones, setPokemones] = useState([])
-
-  useEffect(() => {
-    const getPokemones = async () => {
-
-      const response = await fetch ('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
-      const listaPokemones = await response.json()
-      const { results } = listaPokemones
-     
-      const newPokemones = results.map ( async (pokemon) => {
-        const response = await fetch (pokemon.url)
-        const poke = await response.json()
-
-        return {
-          id: poke.id,
-          name: poke.name,   
-          img: poke.sprites.other.dream_world.front_default
-        }
-    })
-
-    setPokemones(await Promise.all(newPokemones));
-  }
-    getPokemones()
-  }, [])
-
   return (
     <>
     <Navbar />
-    <div className="App">
-      <Buscador />
-        {
-          pokemones.map (pokemon => {
-            return (
-              <div>
-                <img src={pokemon.img} alt={pokemon.name} />
-                <p>{pokemon.name}</p>
-                <span>{pokemon.id}</span>
-              </div>
-            )
-          } )
-        }
-
-      </div>
-      </>)
+    <Buscador />
+    <Pokemones />
+    </>
+  )
 }
 
 export default App
